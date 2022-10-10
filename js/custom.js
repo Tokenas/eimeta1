e6905f8478f0d31a89066fbb4bc8bed11254cd13
Gediminas <30872239+Tokenas@users.noreply.github.com>
2022-10-10 22:16:39 +0300

    Update custom.js

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(".slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    dots: false,
+    autoplay: false,
+});
+
+$(".suppliers-slider").slick({
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    dots: false,
+    autoplay: false,
+});
